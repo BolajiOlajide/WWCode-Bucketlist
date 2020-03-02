d0a77b9f7a5ffdcd72a76101a0a34adf0cac13b3
Bolaji Olajide <cooproton@gmail.com>
2020-03-02 03:00:22 +0100

    feat: refactor migrations

--- migrations/20200302000403_create-user-table.js
+++ migrations/20200302000403_create-user-table.js
@@ -1,19 +1,17 @@
-const logger = require('winston');
-
 const { USER_TABLE_NAME } = require('../constants');
 
 
 exports.up = function(knex) {
   return knex.schema
     .createTable(USER_TABLE_NAME, table => {
-      table.increments().comment('This is the primary key for this table');
-      table.string('username').notNullable().comment('randomly generated ID for this user');
-      table.string('password').notNullable().comment('hashed password belonging to user');
-    }).catch(error => logger.error(error));
+      table.increments();
+      table.string('username').notNullable();
+      table.string('password').notNullable();
+    }).catch(error => console.error(error.message));
 };
 
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists(USER_TABLE_NAME)
-    .catch(error => logger.error(error));
+    .catch(error => console.error(error.message));
 };
--- migrations/20200302012549_create-item-table.js
+++ migrations/20200302012549_create-item-table.js
@@ -1,22 +1,22 @@
-const logger = require('winston');
-
-const { BUCKETLIST_TABLE_NAME, ITEMS_TABLE_NAME } = require('../constants');
+const {
+  BUCKETLIST_TABLE_NAME,
+  ITEMS_TABLE_NAME
+} = require('../constants');
 
 
 exports.up = function(knex) {
   return knex.schema
     .createTable(ITEMS_TABLE_NAME, table => {
-      table.increments().comment('This is the primary key for this table');
-      table.string('name').notNullable().comment('name of the bucketlist');
-      table.boolean('status')
+      table.increments();
+      table.string('name').notNullable();
+      table.boolean('status').notNullable().defaultTo(false);
       table.integer('bucketlist_id').unsigned().references('id')
-        .inTable(BUCKETLIST_TABLE_NAME).onDelete('CASCADE')
-        .comment('the owner of this bucketlist');
-    }).catch(error => logger.error(error));
+        .inTable(BUCKETLIST_TABLE_NAME).onDelete('CASCADE');
+    }).catch(error => console.error(error.message));
 };
 
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists(ITEMS_TABLE_NAME)
-    .catch(error => logger.error(error));
+    .catch(error => console.error(error.message));
 };
